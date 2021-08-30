b3ba9cea3a59a2b22385a1b3899aa30dfb62b8cf
Chris de Graaf <me@cdg.dev>
2021-08-30 02:29:30 -0300

    Stop seeding when something is being played remotely

---
+++ services/plex_qbt/plex_qbt.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+from plexapi.server import PlexServer as Plex
+from qbittorrentapi import Client as Qbt
+
+PLEX_HOST = "http://plex:32400"
+QBT_HOST = "89.36.78.149"
+QBT_PORT = 57419
+
+plex = Plex(PLEX_HOST, os.environ["PLEX_TOKEN"])
+qbt = Qbt(host=QBT_HOST, port=QBT_PORT, username="admin", password=os.environ["QBT_PASSWORD"])
+qbt.auth_log_in()
+
+while True:
+    limit = 0
+    for s in plex.sessions():
+        for p in s.players:
+            if not p.local:
+                limit = 1
+    qbt.transfer_set_upload_limit(limit)
+    time.sleep(10)
